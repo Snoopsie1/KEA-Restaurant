7a772a063594b83051448cab0bbf92051465eac6
Snoopsie1 <oliverrasoli@gmail.com>
2024-02-02 13:17:37 +0100

    dynamic pathing based on hostname

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -5,19 +5,20 @@ class Header extends HTMLElement {
 
 
   connectedCallback() {
+    const hostname = window.location.hostname.includes('github.io') ? '/KEA-Restaurant/' : '/';
     this.innerHTML = `
       <header>
        <h1>Restaurant KEA</h1>
        <nav>
          <ul>
            <li>
-             <a href="/">Home</a>
+             <a href="${hostname}">Home</a>
            </li>
            <li>
-             <a href="/views/menu.html">Menu</a>
+             <a href="${hostname}views/menu.html">Menu</a>
            </li>
            <li>
-             <a href="/views/contact.html">Contact</a>
+             <a href="${hostname}views/contact.html">Contact</a>
            </li>
          </ul>
        </nav>
